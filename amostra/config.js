ecfd391598a258fc8ca5a95fc224baef893e1bdc
eddy14s <eddyestprogramacao@gmail.com>
2026-07-07 23:47:41 -0300

    Tour: AMOSTRA - 07/07/2026 23:47

---
+++ amostra/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  pasta: "imagens_360/",
+  extensao: "jpg",
+  totalImagens: 14, // ⬅️ ajuste aqui só isso
+  senha: "Nuvem465"    // ⬅️ o script de publicação atualiza isso automaticamente
+};
