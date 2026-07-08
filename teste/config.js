208657ed2af155ef3ad8ee504fb41b440ee517e9
eddy14s <eddyestprogramacao@gmail.com>
2026-07-07 21:13:48 -0300

    Tour: TESTE - 07/07/2026 21:13

---
+++ teste/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  pasta: "imagens_360/",
+  extensao: "jpg",
+  totalImagens: 4, // ⬅️ ajuste aqui só isso
+  senha: "Horizonte782"    // ⬅️ o script de publicação atualiza isso automaticamente
+};
---
+++ teste/viewer.js
@@ -0,0 +1,23 @@
+let viewer;
+let currentSceneIndex = 0;
+
+function iniciarTour() {
+  if (viewer && typeof viewer.destroy === "function") {
+    viewer.destroy();
+  }
+
+  viewer = pannellum.viewer('panorama', {
+    default: {
+      firstScene: imagens360[0].id,
+      sceneFadeDuration: 1000,
+      autoLoad: true,
+      showControls: true
+    },
+    scenes: scenes
+  });
+
+  criarMenu();
+  ativarAjuda();
+  criarSetasNavegacao();
+  criarIndicadorCena();
+}
