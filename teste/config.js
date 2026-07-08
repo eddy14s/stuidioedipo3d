c266258507aa6b48a4ba3be5e2c477a8dd0e01f5
eddy14s <eddyestprogramacao@gmail.com>
2026-07-07 22:00:46 -0300

    Tour: TESTE - 07/07/2026 22:00

--- teste/config.js
+++ teste/config.js
@@ -2,5 +2,5 @@ const CONFIG = {
   pasta: "imagens_360/",
   extensao: "jpg",
   totalImagens: 4, // ⬅️ ajuste aqui só isso
-  senha: "Praia808"    // ⬅️ o script de publicação atualiza isso automaticamente
+  senha: "Rocha696"    // ⬅️ o script de publicação atualiza isso automaticamente
 };
