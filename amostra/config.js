c2250eb75726efb60ff24332db5fd705fe0abb30
eddy14s <eddyestprogramacao@gmail.com>
2026-07-13 16:25:37 -0300

    Remove cliente: amostra - 13/07/2026 16:25

--- amostra/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  pasta: "imagens_360/",
-  extensao: "jpg",
-  totalImagens: 14, // ⬅️ ajuste aqui só isso
-  senha: "Nuvem465"    // ⬅️ o script de publicação atualiza isso automaticamente
-};
