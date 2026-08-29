d8b1f95c87285ba31756481d85fa8c5ad1dfb347
eddy14s <eddyestprogramacao@gmail.com>
2026-08-29 01:27:55 -0300

    Tour: TESTE - 29/08/2026 01:27

---
+++ _template/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  pasta: "imagens_360/",
+  extensao: "jpg",
+  totalImagens: 4, // ⬅️ ajuste aqui só isso
+  senha: "Aurora381",    // ⬅️ o script de publicação atualiza isso automaticamente
+  capa: "imagens_360/1.jpg" // ⬅️ imagem mostrada na tela de "Iniciar Tour" (troque pelo render que quiser)
+};
