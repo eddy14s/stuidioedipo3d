74a245c6dface1ecb1127a0370c24300c387c5d7
eddy14s <eddyestprogramacao@gmail.com>
2026-08-29 02:29:14 -0300

    Tour: BANHEIROS - 29/08/2026 02:29

--- banheiros/config.js
+++ banheiros/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   pasta: "imagens_360/",
   extensao: "jpg",
-  totalImagens: 6, // ⬅️ ajuste aqui só isso
+  totalImagens: 5, // ⬅️ ajuste aqui só isso
   senha: "Jasmim433",    // ⬅️ o script de publicação atualiza isso automaticamente
   capa: "imagens_360/1.jpg" // ⬅️ imagem mostrada na tela de "Iniciar Tour" (troque pelo render que quiser)
 };
