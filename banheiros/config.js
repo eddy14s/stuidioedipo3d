1b62bbc3a50c65da6ca31d068d23a47bcf8fe53e
eddy14s <eddyestprogramacao@gmail.com>
2026-08-30 00:13:51 -0300

    Revise bathroom titles in config.js
    Updated titles for the bathrooms in the configuration.

--- banheiros/config.js
+++ banheiros/config.js
@@ -1,14 +1,15 @@
 const CONFIG = {
   pasta: "imagens_360/",
   extensao: "jpg",
-  totalImagens: 5, // ⬅️ ajuste aqui só isso
-  senha: "Jasmim433",    // ⬅️ o script de publicação atualiza isso automaticamente
-  capa: "imagens_360/1.jpg" // ⬅️ imagem mostrada na tela de "Iniciar Tour" (troque pelo render que quiser)
+  totalImagens: 5,
+  senha: "Jasmim433",
+  capa: "imagens_360/1.jpg",
+  titulos: {
+    1: "Banho Piscina",
+    2: "Lavabo",
+    3: "Banho Suíte01",
+    4: "Banho Hospedes"
+    4: "Banho 03"
+    
+  }
 };
-
-titulos: {
-  1: "Banho 1",
-  2: "Banho 2",
-  3: "Suíte",
-  4: "Cozinha"
-}
