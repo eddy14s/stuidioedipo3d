f3eaef8df8816734aed80cd77e0ba208edf7b5f8
eddy14s <eddyestprogramacao@gmail.com>
2026-08-30 00:03:48 -0300

    Add titles for different rooms in config.js

--- banheiros/config.js
+++ banheiros/config.js
@@ -5,3 +5,10 @@ const CONFIG = {
   senha: "Jasmim433",    // ⬅️ o script de publicação atualiza isso automaticamente
   capa: "imagens_360/1.jpg" // ⬅️ imagem mostrada na tela de "Iniciar Tour" (troque pelo render que quiser)
 };
+
+titulos: {
+  1: "Banho 1",
+  2: "Banho 2",
+  3: "Suíte",
+  4: "Cozinha"
+}
