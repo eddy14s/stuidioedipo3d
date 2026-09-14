59b031f8d729417a25d5f467d0ea2e43457463b2
eddy14s <eddyestprogramacao@gmail.com>
2026-09-13 21:53:28 -0300

    Tour: RES_JAQUELINE - 13/09/2026 21:53

--- res-jaqueline/config.js
+++ res-jaqueline/config.js
@@ -5,15 +5,14 @@ const CONFIG = {
   senha: "Serra917",
   capa: "imagens_360/1.jpg",
   titulos: {
-    1: "SALA_V1",
-    2: "SALA_V2",
+    1: "LIVING",
+    2: "CIRCULAÇÃO",
     3: "COZINHA",
     4: "LAVANDERIA",
     5: "LAVABO",
     6: "BANHEIRO_01",
     7: "BANHEIRO_02",
     8: "BANHEIRO_03",
-    9: "BANHEIRO_05",
-    10: "BANHEIRO_05"
+    9: "BANHEIRO_05"
   }
 };
