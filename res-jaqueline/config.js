ebb4216c58d8256c079559037fdafedd70cef955
eddy14s <eddyestprogramacao@gmail.com>
2026-09-15 00:02:37 -0300

    Add files via upload

--- res-jaqueline/config.js
+++ res-jaqueline/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     // defina aqui apenas as imagens que fogem do padrao acima
     // exemplo: 3: "png"
   },
-  totalImagens: 9,
+  totalImagens: 10,
   senha: "Jasmim433",
   capa: "imagens_360/1.jpg",
   titulos: {
@@ -15,14 +15,14 @@ const CONFIG = {
     4: "Banho Hospedes",
     5: "Banho 03"
   },
-posicoes: {
-    1: { yaw: -90, pitch: -3 },
-    2: { yaw: 127, pitch: -2 },
-    3: { yaw: 91, pitch: 0 },
-    5: { yaw: -126, pitch: -12 },
-    6: { yaw: -55, pitch: -10 },
-    7: { yaw: 145, pitch: -10 },
-    8: { yaw: -125, pitch: -10 },
-    9: { yaw: -13, pitch: -27 }
+  posicoes: {
+    1: { yaw: 0, pitch: 0 },
+    2: { yaw: 0, pitch: 0 },
+    3: { yaw: 0, pitch: 0 },
+    4: { yaw: 0, pitch: 0 },
+    5: { yaw: 0, pitch: 0 },
+    6: { yaw: 0, pitch: 0 },
+    7: { yaw: 0, pitch: 0 }
   }
+};
 
