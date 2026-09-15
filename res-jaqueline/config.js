ef8298492370801252b858246952dae278d2d2b0
eddy14s <eddyestprogramacao@gmail.com>
2026-09-15 00:03:07 -0300

    Update config.js

--- res-jaqueline/config.js
+++ res-jaqueline/config.js
@@ -15,14 +15,15 @@ const CONFIG = {
     4: "Banho Hospedes",
     5: "Banho 03"
   },
-  posicoes: {
-    1: { yaw: 0, pitch: 0 },
-    2: { yaw: 0, pitch: 0 },
-    3: { yaw: 0, pitch: 0 },
-    4: { yaw: 0, pitch: 0 },
-    5: { yaw: 0, pitch: 0 },
-    6: { yaw: 0, pitch: 0 },
-    7: { yaw: 0, pitch: 0 }
+posicoes: {
+    1: { yaw: -90, pitch: -3 },
+    2: { yaw: 127, pitch: -2 },
+    3: { yaw: 91, pitch: 0 },
+    5: { yaw: -126, pitch: -12 },
+    6: { yaw: -55, pitch: -10 },
+    7: { yaw: 145, pitch: -10 },
+    8: { yaw: -125, pitch: -10 },
+    9: { yaw: -13, pitch: -27 }
   }
-};
+
 
