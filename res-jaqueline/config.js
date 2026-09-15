95762a33aea01c1bff02e75cfd54683742c5ca5e
eddy14s <eddyestprogramacao@gmail.com>
2026-09-15 00:06:24 -0300

    Decrease totalImages in config.js from 10 to 9
    Updated totalImages from 10 to 9 in the configuration.

--- res-jaqueline/config.js
+++ res-jaqueline/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     // defina aqui apenas as imagens que fogem do padrao acima
     // exemplo: 3: "png"
   },
-  totalImagens: 10,
+  totalImagens: 9,
   senha: "Jasmim433",
   capa: "imagens_360/1.jpg",
   titulos: {
